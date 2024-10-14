b44b2fa7f55c8d2fc864dee990debfe8c03f1505
Sergi Botargues <sbotargues@gmail.com>
2024-10-14 22:05:51 +0200

    microfrontend

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
+import webpack, { Configuration as WebpackConfig } from "webpack";
+
 export default defineNuxtConfig({
   modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
   css: ["~/assets/css/tailwind.css"],
+
   runtimeConfig: {
     public: {
       apiUrl: process.env.API_URL || "http://localhost:8000/api",
     },
   },
+
   build: {
     postcss: {
       plugins: {
@@ -13,5 +17,36 @@ export default defineNuxtConfig({
         autoprefixer: {},
       },
     },
+    extend(config: WebpackConfig, { isClient }: { isClient: boolean }) {
+      if (isClient) {
+        if (!config.plugins) {
+          config.plugins = [];
+        }
+
+        config.plugins.push(
+          new webpack.container.ModuleFederationPlugin({
+            name: "taskManagerFrontend",
+            filename: "remoteEntry.js",
+            exposes: {
+              "./TaskApp": "./pages/index.vue",
+            },
+            shared: {
+              vue: {
+                singleton: true,
+                eager: true,
+                requiredVersion: false,
+              },
+              pinia: {
+                singleton: true,
+                eager: true,
+                requiredVersion: false,
+              },
+            },
+          })
+        );
+      }
+    },
   },
+
+  compatibilityDate: "2024-10-14",
 });
